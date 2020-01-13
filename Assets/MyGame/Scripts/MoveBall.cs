469499bfbefb7a86ca5b0e800796e68aea63a274
aweng <annawenger@gmx.at>
2020-01-13 17:14:36 +0100

    coding

--- Assets/MyGame/Scripts/MoveBall.cs
+++ Assets/MyGame/Scripts/MoveBall.cs
@@ -1,18 +1,11 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class scr : MonoBehaviour
+public class MoveBall : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Transform[] positions;
 
-    // Update is called once per frame
-    void Update()
+    public void RandomBallPosition()
     {
-        
+        transform.position = positions[Random.Range(0, positions.Length)].position;
     }
 }
